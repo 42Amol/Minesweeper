7a9c046dd3781f6226fd4d27c320f2af3a61a2cc
Amol <n/a>
2022-08-25 18:32:18 +0100

    cell struct array (segfault atm)

--- main.c
+++ main.c
@@ -5,40 +5,41 @@ const int HEIGHT = 10;
 const int WIDTH = 8;
 const int MINES = 15;
 
+struct Cell {
+  char symbol;
+  short hidden;
+};
 
-void intro()
-{
+
+void intro() {
   printf("Welcome to Minesweeper!\n\n");
 }
 
-void init_board(int board[HEIGHT][WIDTH])
-{
-  for (int i = 0; i < HEIGHT; i++)
-  {
-    for (int j = 0; j < WIDTH; j++)
-    {
-      board[i][j] = 0;
+void init_board(struct Cell board[HEIGHT][WIDTH]) {
+  for (int i = 0; i < HEIGHT; i++) {
+    for (int j = 0; j < WIDTH; j++) {
+      board[i][j].symbol = '-';
+      board[i][j].hidden = 1;
     }
   } 
 }
 
 
-void print_board(int board[HEIGHT][WIDTH])
-{
-  for (int i = 0; i < HEIGHT; i++)
-  {
-    for (int j = 0; j < WIDTH; j++)
-    {
-      printf(" %d ", board[i][j]);
+void print_board(struct Cell board[HEIGHT][WIDTH]) {
+  for (int i = 0; i < HEIGHT; i++) {
+    for (int j = 0; j < WIDTH; j++) {
+      printf(" %s ", board[i][j].hidden == 0 ? board[i][j].symbol : '#');
     }
     printf("\n");
   } 
 }
 
 int main() {
-  int board[HEIGHT][WIDTH];
-
+  struct Cell board[HEIGHT][WIDTH];
+  
+  printf("board initialization ...\n");
   init_board(board);
+  printf("board printing ...\n");
   print_board(board);
 
   return 0;
