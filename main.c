7e0a90f705af5fc5fb56fee8091a1a47a126e985
Amol <adresse.site.comptes@gmail.com>
2022-08-26 15:05:31 +0100

    segfault fixed, normal board printing

--- main.c
+++ main.c
@@ -2,20 +2,36 @@
 
 
 const int HEIGHT = 10;
-const int WIDTH = 8;
+const int WIDTH = 10;
 const int MINES = 15;
 
-struct Cell {
+typedef struct Cell {
   char symbol;
   short hidden;
-};
+} cell;
+
+
+void intro();
+void init_board(cell board[HEIGHT][WIDTH]);
+void print_board(cell board[HEIGHT][WIDTH]);
+
+int main() {
+  cell board[HEIGHT][WIDTH];
+  
+  printf("board initialization ...\n");
+  init_board(board);
+  printf("board printing ...\n");
+  print_board(board);
+
+  return 0;
+}
 
 
 void intro() {
   printf("Welcome to Minesweeper!\n\n");
 }
 
-void init_board(struct Cell board[HEIGHT][WIDTH]) {
+void init_board(cell board[HEIGHT][WIDTH]) {
   for (int i = 0; i < HEIGHT; i++) {
     for (int j = 0; j < WIDTH; j++) {
       board[i][j].symbol = '-';
@@ -24,23 +40,10 @@ void init_board(struct Cell board[HEIGHT][WIDTH]) {
   } 
 }
 
-
-void print_board(struct Cell board[HEIGHT][WIDTH]) {
+void print_board(cell board[HEIGHT][WIDTH]) {
   for (int i = 0; i < HEIGHT; i++) {
-    for (int j = 0; j < WIDTH; j++) {
-      printf(" %s ", board[i][j].hidden == 0 ? board[i][j].symbol : '#');
-    }
+    for (int j = 0; j < WIDTH; j++) 
+      printf(" %c", board[i][j].hidden == 0 ? board[i][j].symbol : '#');
     printf("\n");
   } 
-}
-
-int main() {
-  struct Cell board[HEIGHT][WIDTH];
-  
-  printf("board initialization ...\n");
-  init_board(board);
-  printf("board printing ...\n");
-  print_board(board);
-
-  return 0;
 }
