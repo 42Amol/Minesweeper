c6b1e3543c428eff674f339d601fb81edecfb7da
Amol <adresse.site.comptes@gmail.com>
2022-08-27 22:02:07 +0100

    reformat to comply with coding style + `reveal_board` function

--- main.c
+++ main.c
@@ -5,45 +5,56 @@ const int HEIGHT = 10;
 const int WIDTH = 10;
 const int MINES = 15;
 
-typedef struct Cell {
-  char symbol;
-  short hidden;
-} cell;
+typedef struct {
+    char symbol;
+    short hidden;
+} cell_t;
 
 
-void intro();
-void init_board(cell board[HEIGHT][WIDTH]);
-void print_board(cell board[HEIGHT][WIDTH]);
+void init_board(cell_t board[HEIGHT][WIDTH]);
+void display_board(cell_t board[HEIGHT][WIDTH]);
+void reveal_board(cell_t board[HEIGHT][WIDTH]);
 
-int main() {
-  cell board[HEIGHT][WIDTH];
+
+int main()
+{
+    cell_t board[HEIGHT][WIDTH];
   
-  printf("board initialization ...\n");
-  init_board(board);
-  printf("board printing ...\n");
-  print_board(board);
+    init_board(board);
+    display_board(board);
 
-  return 0;
+    return 0;
 }
 
 
-void intro() {
-  printf("Welcome to Minesweeper!\n\n");
+void init_board(cell_t board[HEIGHT][WIDTH])
+{
+    for (int i = 0; i < HEIGHT; i++)
+    {
+        for (int j = 0; j < WIDTH; j++)
+        {
+          board[i][j].symbol = '0';
+          board[i][j].hidden = 1;
+        }
+    } 
 }
 
-void init_board(cell board[HEIGHT][WIDTH]) {
-  for (int i = 0; i < HEIGHT; i++) {
-    for (int j = 0; j < WIDTH; j++) {
-      board[i][j].symbol = '-';
-      board[i][j].hidden = 1;
-    }
-  } 
+void display_board(cell_t board[HEIGHT][WIDTH])
+{
+    for (int i = 0; i < HEIGHT; i++)
+    {
+        for (int j = 0; j < WIDTH; j++) 
+            printf(" %c", board[i][j].hidden == 0 ? board[i][j].symbol : '-');
+        printf("\n");
+    } 
 }
 
-void print_board(cell board[HEIGHT][WIDTH]) {
-  for (int i = 0; i < HEIGHT; i++) {
-    for (int j = 0; j < WIDTH; j++) 
-      printf(" %c", board[i][j].hidden == 0 ? board[i][j].symbol : '#');
-    printf("\n");
-  } 
+void reveal_board(cell_t board[HEIGHT][WIDTH])
+{
+    for (int i = 0; i < HEIGHT; i++)
+    {
+        for (int j = 0; j < WIDTH; j++) 
+            printf(" %c", board[i][j].symbol);
+        printf("\n");
+    } 
 }
