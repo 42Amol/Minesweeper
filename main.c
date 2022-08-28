233bbbd281fe5226f8f6aea704099a1632ad9afa
Amol <adresse.site.comptes@gmail.com>
2022-08-28 15:50:05 +0100

    full board generator

--- main.c
+++ main.c
@@ -14,6 +14,7 @@ typedef struct {
 
 void generate_board(cell_t board[HEIGHT][WIDTH]);
 void place_mine(cell_t board[HEIGHT][WIDTH], int x, int y);
+void increase_cell_value(cell_t board[HEIGHT][WIDTH], int x, int y);
 void display_board(cell_t board[HEIGHT][WIDTH]);
 void reveal_board(cell_t board[HEIGHT][WIDTH]);
 
@@ -23,7 +24,7 @@ int main()
     cell_t board[HEIGHT][WIDTH];
   
     generate_board(board);
-    display_board(board);
+    reveal_board(board);
 
     return 0;
 }
@@ -31,7 +32,7 @@ int main()
 //// GENERATION
 
 /*
-** Place mines on board and match surrounding cells
+** Fill board with randomly placed mines
 */
 void generate_board(cell_t board[HEIGHT][WIDTH])
 {
@@ -43,9 +44,32 @@ void generate_board(cell_t board[HEIGHT][WIDTH])
           board[i][j].symbol = '0';
           board[i][j].hidden = 1;
         }
-    } 
+    }
+
+    // random number generation (based on Floyd algorithm)
+    // generate a set of `MINES` unique positive number < `SIZE`
+
+    const int SIZE = HEIGHT * WIDTH;
+    int i_m = 0;
+    char is_used[SIZE];
+    
+    for (int i = 0; i < SIZE; i++)
+        is_used[i] = 0;
+
+    for (int i_s = SIZE - MINES; i_s < SIZE && i_m < MINES; ++i_s, ++i_m) {
+        int r = rand() % i_s;
+
+        if (is_used[r])
+            r = i_s; // use 'i_s' instead of the generated number
+
+        place_mine(board, r / HEIGHT, r % WIDTH);
+        is_used[r] = 1;
+    }
 }
 
+/*
+** Place a mine at given coordinates (x vertical, y horizontal)
+*/
 void place_mine(cell_t board[HEIGHT][WIDTH], int x, int y)
 {
     board[x][y].symbol = 'X';
@@ -56,10 +80,13 @@ void place_mine(cell_t board[HEIGHT][WIDTH], int x, int y)
                 increase_cell_value(board, x + i, y + j);
 }
 
+/*
+** If an empty cell exists at given coordinates, increase its value
+*/
 void increase_cell_value(cell_t board[HEIGHT][WIDTH], int x, int y)
 {
-    if (x >= 0 && y >= 0 && x < HEIGHT && y < WIDTH && cell_t[x][y].symbol != 'X')
-        cell_t[x][y].symbol++;
+    if (x >= 0 && y >= 0 && x < HEIGHT && y < WIDTH && board[x][y].symbol != 'X')
+        board[x][y].symbol++;
 }
 
 
